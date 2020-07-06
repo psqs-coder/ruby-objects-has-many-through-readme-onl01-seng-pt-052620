8ef0718c982b6f359907abd700524415fc31cbbc
Bruce Parker <detailsharks@gmail.com>
2020-07-06 23:48:44 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,6 +1,10 @@
 class Customer
   attr_accessor :name, :age
   
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+  
   @@all = []
   
   def initialize(name,age)
